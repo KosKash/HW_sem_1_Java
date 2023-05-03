6e2fca20795c2ae30a71a2a302f193eab88dac8c
Constantine <koskash98ksband@gmail.com>
2023-05-03 22:54:54 +0600

    Done

--- Ex2.java
+++ Ex2.java
@@ -1,16 +1,72 @@
+import java.util.Scanner;
 public class Ex2 {
     public static void main(String[] args) {
-        System.out.println("Все простые числа от 1 до 1000");
-        for (int i = 2; i < 1000; i++) {
-            boolean flag = false;
-            for (int j = 2; j < i-1; j++) {
-                    if (i % j == 0) {
-                        flag = true;
-                    }
-            }
-            if (flag == false) {
-                System.out.print(i+" ");
+        Calc();
+    }
+
+    public static double Input_n() {
+        Scanner iScanner = new Scanner(System.in);
+        System.out.println("Num");
+        double num = iScanner.nextInt();
+        return num;
+    }
+
+    public static String Input_op() {
+        Scanner iScanner = new Scanner(System.in);
+        System.out.println("Oper");
+        String o = iScanner.next();
+        return o;
+    }
+
+    public static void Calc() {
+        double num_1 = Input_n();
+        String op = Input_op();
+        double num_2 = Input_n();
+        double result = 0;
+        switch (op) {
+            case "+":
+                result = num_1 + num_2;
+                break;
+            case "-":
+                result = num_1 - num_2;
+                break;
+            case "*":
+                result = num_1 * num_2;
+                break;
+            case "/":
+                result = num_1 / num_2;
+                break;
+            default:
+                System.out.printf("Error! Enter correct operator");
+                return;
+        }
+        boolean out = false;
+        while (!out) {
+            System.out.println(result);
+            op = Input_op();
+            num_1 = Input_n();
+            switch (op) {
+                case "+":
+                    result = result + num_1;
+                    break;
+                case "-":
+                    result = result - num_1;
+                    break;
+                case "*":
+                    result = result * num_1;
+                    break;
+                case "/":
+                    result = result / num_1;
+                    break;
+                case "":
+                    out = true;
+                
+                default:
+                    System.out.printf("Error! Enter correct operator");
+                    return;
             }
         }
+        System.out.println(result);
     }
+
 }
